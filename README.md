a5824e9f0a0c3c98e2a5a99b9558365fef2d5ab3
hululu9842 <114321565+hululu9842@users.noreply.github.com>
2023-10-31 20:58:05 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # url_screen
-域名或url筛选工具  最近利用 JSFinder-master 扫描时，发现找到的很多url以及域名都不能用，所以写了一个很简单的python来简单解决一下，用来筛选200状态、需要的主域名以及去掉不必要的图片等的url。  通常配合 JSFinder-master使用。  仅供小白参考。
+
+域名或url筛选工具
+
+最近利用 JSFinder-master 扫描时，发现找到的很多url以及域名都不能用，所以写了一个很简单的python来简单解决一下，用来筛选200状态、需要的主域名以及去掉不必要的图片等的url。
+
+通常配合 JSFinder-master使用。
+
+仅供小白参考。
+
+`python url_screen.py -h  ` 查看帮助
+
+```
+optional arguments:
+  -h, --help            show this help message and exit
+  --domain DOMAIN [DOMAIN ...]
+                        主域名,如 baidu.com，如果是多个，用空格隔开
+  --last LAST [LAST ...]
+                        去掉图片等文件的url，如果是多个，用空格隔开,如使用 .png .jpg
+  --put PUT             输入的url文件
+  --out OUT             输出的url文件
+```
+
+常用方法：
+
+`python url_screen.py --domain xxx.com yyy.com --last .jpg .png .gif --put E:\url.txt --out E:\urls.txt`
+
+![3p3aj7.png](https://files.catbox.moe/3p3aj7.png)
---
+++ url_screen.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import re
+import requests
+import urllib3
+import argparse
+
+def parser():
+    parser = argparse.ArgumentParser(description='manual to this script')
+    parser.add_argument("--domain",nargs='+',help='主域名,如 baidu.com，如果是多个，用空格隔开')
+    parser.add_argument("--last",nargs='+',help='去掉图片等文件的url，如果是多个，用空格隔开,如使用 .png .jpg')
+    parser.add_argument("--put", help='输入的url文件')
+    parser.add_argument("--out", help='输出的url文件')
+    args = parser.parse_args()
+    return args
+
+
+def get_code_status(urls_file,domain,last):
+    """
+    检测urls状态码
+    :param urls_file: urls文件
+    :return: 状态码
+    allow_redirects: 拒绝默认的301/302重定向从而可以通过 html.headers[‘Location’] 拿到重定向的 URL。
+    verify: 取消证书认证
+    """
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3298.4 Safari/537.36'
+    }
+    domains = "|".join(domain)
+    domains = domains.replace('.', '\.')
+    lasts = "|".join(last)
+    lasts = lasts.replace('.','\.')
+    with open(urls_file, 'r', encoding='utf-8') as f:
+        urls_data = f.readlines()
+        result = ''
+        for url in urls_data:
+            ret_1 = re.findall(domains, url)  # 正则匹配主域名
+            ret_2 = re.findall(lasts, url)  # 正则匹配文件url的后缀
+            url = url.strip('\n')
+            disable_warnings()
+            try:
+                res = requests.get(
+                    url, headers=header, verify=False, allow_redirects=False
+                )
+            except Exception as error:
+                print(error)
+            code = res.status_code
+            if code == 200:
+                if ret_1!=[]:
+                    if ret_2==[]:
+                        print('状态码 {}:{}'.format(code, url))
+                        result=result+''.join(url)
+    return result
+
+
+def disable_warnings():
+    """
+    解除去掉证书后总是抛出异常告警
+    """
+    urllib3.disable_warnings()
+
+
+if __name__ == '__main__':
+    arg=parser()
+    results=get_code_status(arg.put,arg.domain,arg.last)
+    m = open(arg.out, 'w').write(results)
+
